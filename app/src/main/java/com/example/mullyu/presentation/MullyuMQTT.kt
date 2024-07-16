a767a20650529a5c457d49f9273b257a3c27a445
qazw181900 <qazw181900@naver.com>
2024-07-16 11:26:08 +0900

    feat : mqtt publish
    mqtt connect, publish implementation

--- app/src/main/java/com/example/mullyu/presentation/MullyuMQTT.kt
+++ app/src/main/java/com/example/mullyu/presentation/MullyuMQTT.kt
@@ -1,5 +1,8 @@
 package com.example.mullyu.presentation
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.eclipse.paho.client.mqttv3.MqttCallback
 import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
@@ -7,28 +10,37 @@ import org.eclipse.paho.client.mqttv3.MqttException
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import java.util.UUID
 
-class MullyuMQTT(){//callback: MqttCallback?) {
+class MullyuMQTT(callback: MqttCallback?) {
     private val mqttClient: MqttClient
-    private val brokerUrl = "tcp://test.mosquitto.org:1883"//"tcp://iot.eclipse.org:1883"//"tcp://test.mosquitto.org:1883"
+    private val brokerUrl = "tcp://192.168.56.1:1883"//"tcp://broker.emqx.io:1883"//"tcp://broker.hivemq.com:1883"//"tcp://iot.eclipse.org:1883"//"tcp://test.mosquitto.org:1883"
     private val clientId = UUID.randomUUID().toString() // UUID 사용
 
     init {
         mqttClient = MqttClient(brokerUrl, clientId, null) // null을 전달
-        //mqttClient.setCallback(callback)
+        mqttClient.setCallback(callback)
     }
 
     fun connect() {
-        try {
-            val options = MqttConnectOptions()
-            options.isCleanSession = true
-            mqttClient.connect(options)
-            println("MQTT 연결 시도")
-            if (mqttClient.isConnected) {
-                println("MQTT 연결 성공")
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val options = MqttConnectOptions().apply {
+                    isCleanSession = true
+                    connectionTimeout = 10  // 10초 타임아웃 설정
+                }
+                options.isCleanSession = true
+                println("MQTT connect try")
+                mqttClient.connect(options)
+                println("MQTT connect success")
+                println("MQTT 연결 시도")
+                if (mqttClient.isConnected) {
+                    println("MQTT 연결 성공")
+                } else {
+                    println("MQTT 연결 실패")
+                }
+            } catch (e: MqttException) {
+                println("MQTT 연결 실패: ${e.message}")
+                e.printStackTrace()
             }
-        } catch (e: MqttException) {
-            println("MQTT 연결 실패: ${e.message}")
-            e.printStackTrace()
         }
     }
 
